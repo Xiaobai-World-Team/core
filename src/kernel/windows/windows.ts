fe606b84aadf3886da7521a45b3856447811c2ea
xiaobai <xiaobai-world@foxmail.com>
2021-05-09 23:34:08 +0800

    add drag and move

--- src/kernel/windows/windows.ts
+++ src/kernel/windows/windows.ts
@@ -1,11 +1,31 @@
 import { ref } from "@vue/reactivity";
 
+/** 
+ * on desktop have mutiple windows, every window has below "IWindow interface",
+ * each window be the body's directly child
+ * */
 interface IWindow {
-  id: number;
+  id: string;
   x: number
   y: number
   width: number
   height: number
 }
 
-const windows = ref<IWindow[]>([])
+export const windows = ref<IWindow[]>([])
+
+windows.value.push({
+  id: Date.now().toString(36) + Math.random().toString(36).substring(2, 20),
+  x: 100,
+  y: 160,
+  width: 300,
+  height: 300
+})
+
+windows.value.push({
+  id: Date.now().toString(36) + Math.random().toString(36).substring(2, 20),
+  x: 500,
+  y: 200,
+  width: 500,
+  height: 200
+})
--- src/kernel/windows/windows.vue
+++ src/kernel/windows/windows.vue
@@ -1,10 +1,258 @@
 <template>
-  <div style="position: absolute; left: 0; top: 0; background: red">123</div>
+  <div
+    v-for="window in windows"
+    :id="window.id"
+    :key="window.id"
+    class="xiaobai-window-resize"
+    :style="{
+      position: 'fixed',
+      left: window.x + 'px',
+      top: window.y + 'px',
+      width: window.width + 'px',
+      height: window.height + 'px',
+      border: 'solid 2px red',
+    }"
+  >
+    <pre>{{ window }}</pre>
+    <div xiaobai-resize-trigger class="toolbar"></div>
+    <div xiaobai-resize-trigger class="e"></div>
+    <div xiaobai-resize-trigger class="s"></div>
+    <div xiaobai-resize-trigger class="w"></div>
+    <div xiaobai-resize-trigger class="n"></div>
+    <div xiaobai-resize-trigger class="ne"></div>
+    <div xiaobai-resize-trigger class="nw"></div>
+    <div xiaobai-resize-trigger class="sw"></div>
+    <div xiaobai-resize-trigger class="se"></div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import { windows } from "./windows";
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    return {
+      windows: windows,
+    };
+  },
+  mounted() {
+    function preventDefault(ev: Event) {
+      ev.preventDefault();
+    }
+
+    document.addEventListener("mousedown", function (ev) {
+      if (ev.target === null) {
+        return;
+      }
+      const target: HTMLDivElement = <HTMLDivElement>ev.target;
+
+      if (target.getAttribute("xiaobai-resize-trigger") === null) {
+        return;
+      }
+
+      ev.preventDefault();
+      ev.stopPropagation();
+
+      const parent = <HTMLDivElement>target.parentNode;
+      const id = parent.id;
+
+      const windowIdx = windows.value.findIndex((item) => {
+        return item.id === id;
+      });
+
+      const obj = windows.value[windowIdx];
+
+      // save init point
+      var _initPoint = {
+        left: obj.x,
+        top: obj.y,
+        width: obj.width,
+        height: obj.height,
+      };
+
+      var initX = ev.pageX;
+      var initY = ev.pageY;
+      var startX = _initPoint.left;
+      var startY = _initPoint.top;
+      var endX = _initPoint.left + _initPoint.width;
+      var endY = _initPoint.top + _initPoint.height;
+
+      bind();
+
+      var direction = target.className;
+
+      function bind() {
+        document.addEventListener("selectstart", preventDefault);
+        document.addEventListener("dragstart", preventDefault);
+
+        // save coordinates during movement
+        var point = {
+          startX: _initPoint.left,
+          startY: _initPoint.top,
+          endX: endX,
+          endY: endY,
+        };
+
+        function mousemove(ev: MouseEvent) {
+          switch (direction) {
+            case "e":
+              point.endX = endX + (ev.pageX - initX);
+              break;
+            case "s":
+              point.endY = endY + (ev.pageY - initY);
+              break;
+            case "w":
+              point.startX = startX - (initX - ev.pageX);
+              break;
+            case "n":
+              point.startY = startY - (initY - ev.pageY);
+              break;
+            case "nw":
+              point.startX = startX - (initX - ev.pageX);
+              point.startY = startY - (initY - ev.pageY);
+              break;
+            case "sw":
+              point.startX = startX - (initX - ev.pageX);
+              point.endY = endY + (ev.pageY - initY);
+              break;
+            case "se":
+              point.endX = endX + (ev.pageX - initX);
+              point.endY = endY + (ev.pageY - initY);
+              break;
+            case "ne":
+              point.endX = endX + (ev.pageX - initX);
+              point.startY = startY - (initY - ev.pageY);
+              break;
+            case "toolbar":
+              obj.x = point.startX + (ev.pageX - initX);
+              obj.y = point.startY + (ev.pageY - initY);
+              return;
+          }
+
+          var newPoint = {
+            left: point.startX < point.endX ? point.startX : point.endX,
+            top: point.startY < point.endY ? point.startY : point.endY,
+            width:
+              point.startX < point.endX
+                ? point.endX - point.startX
+                : point.startX - point.endX,
+            height:
+              point.startY < point.endY
+                ? point.endY - point.startY
+                : point.startY - point.endY,
+          };
+
+          obj.x = newPoint.left;
+          obj.y = newPoint.top;
+          obj.width = newPoint.width;
+          obj.height = newPoint.height;
+        }
+        document.addEventListener("mousemove", mousemove);
+
+        function mouseup() {
+          document.removeEventListener("selectstart", preventDefault);
+          document.removeEventListener("dragstart", preventDefault);
+          document.removeEventListener("mousemove", mousemove);
+          document.removeEventListener("mouseup", mouseup);
+        }
+
+        document.addEventListener("mouseup", mouseup);
+      }
+    });
+  },
 });
 </script>
+
+<style lang="less">
+.xiaobai-window-resize > [xiaobai-resize-trigger] {
+  position: absolute;
+  box-sizing: border-box;
+  border: solid 1px rgba(0, 0, 0, 0.6);
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].toolbar {
+  height: 30px;
+  width: 100%;
+  background: red;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].e {
+  right: -6px;
+  top: 0;
+  bottom: 0;
+  width: 8px;
+  background: rgba(255, 255, 255, 0.5);
+  cursor: e-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].s {
+  left: 0;
+  bottom: -6px;
+  right: 0;
+  height: 8px;
+  background: rgba(2, 208, 255, 0.5);
+  cursor: s-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].w {
+  left: -6px;
+  bottom: 0;
+  top: 0;
+  width: 8px;
+  background: rgba(255, 27, 50, 0.5);
+  cursor: w-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].n {
+  left: 0;
+  top: -6px;
+  right: 0;
+  height: 8px;
+  background: rgba(255, 255, 0, 0.4);
+  cursor: n-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].ne,
+.xiaobai-window-resize > [xiaobai-resize-trigger].nw,
+.xiaobai-window-resize > [xiaobai-resize-trigger].sw,
+.xiaobai-window-resize > [xiaobai-resize-trigger].se {
+  border-radius: 50% 50%;
+  width: 14px;
+  height: 14px;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].ne {
+  right: -7px;
+  top: -7px;
+  background: #fff;
+  z-index: 2;
+  cursor: ne-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].nw {
+  left: -7px;
+  top: -7px;
+  background: #fff;
+  z-index: 2;
+  cursor: nw-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].sw {
+  left: -7px;
+  bottom: -7px;
+  background: #fff;
+  z-index: 2;
+  cursor: sw-resize;
+}
+
+.xiaobai-window-resize > [xiaobai-resize-trigger].se {
+  right: -7px;
+  bottom: -7px;
+  background: #fff;
+  z-index: 2;
+  cursor: se-resize;
+}
+</style>
