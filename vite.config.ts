6b22f00f9e121a4416171c3e7177f32fb4e5cbfd
xiaobai <xiaobai-world@foxmail.com>
2021-09-04 19:43:25 +0800

    modify base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://xiaobai-world.oss-cn-hangzhou.aliyuncs.com/core-assets',
+  base: 'https://xiaobai-world.oss-cn-hangzhou.aliyuncs.com/core-backend',
   server: {
     port: 3005,
     proxy: {
